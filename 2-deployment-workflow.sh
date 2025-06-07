79070e440e62334a901a3ee811d5c925749aef10
mrjex <joel.mattsson@hotmail.se>
2025-06-07 16:47:05 +0200

    Add deployment charm workflow

---
+++ 2-deployment-workflow.sh
@@ -0,0 +1,10 @@
+# Deploy the Charm Operator
+
+charmcraft pack
+juju deploy ./ripgrep-operator_ubuntu-20.04-amd64.charm
+
+# Check status
+juju status
+
+# Run an analysis
+juju run-action ripgrep-operator/0 analyze package=nginx pattern="version"

8cfac6485a46952f2326c6b1d320330808701c2e
mrjex <joel.mattsson@hotmail.se>
2025-06-11 00:19:26 +0200

    Add Bash automation setup for snap packages

---
+++ get-snap-packages.sh
@@ -0,0 +1,25 @@
+# This script gets all Snap packages from the shared Multipass VM.
+
+# TODO
+
+
+
+# Get Snap package from the private CLI repository
+getPrivateCLI() {
+    echo "TODO"
+}
+
+
+
+# Get Snap package of the Linux distro CLI
+getLinuxDistroCLI() {
+    echo "TODO"
+}
+
+
+
+
+# Formats the package names to conform to the Ripgrep Operator's naming conventions
+formatPackage() {
+    echo "TODO"
+}
